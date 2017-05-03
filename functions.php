aa79af7700b493a6a90eccb2d2d8c5fb625897aa
fokkev <fokkeveenstra@hotmail.com>
2017-05-03 14:41:13 +0200

    tr

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/user-name/repo-name/',
+	__FILE__,
+	'unique-plugin-or-theme-slug'
+);
 
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('stable-branch-name');
 
 
 
